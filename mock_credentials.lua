b08025d0de2c3c14ebfb865862d437b8427c69df
Nick Speal <nick@speal.ca>
2017-12-12 19:03:57 +0530

    add mock_credentials file

---
+++ mock_credentials.lua
@@ -0,0 +1,12 @@
+-- WIFI SETTINGS
+-- Variable Names must be capital letters!!!
+SSID = ""
+PASSWORD = ""
+
+-- SENSOR NAME
+-- Format facility-gender-number
+-- ie. nemua-men-1
+SENSOR_ID = "nemua-men-x"
+
+-- Online server Setting
+UBIDOTS_TOKEN = ""
